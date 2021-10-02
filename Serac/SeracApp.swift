ef27e72eba1aa3f586ee095218d2a0ae12ca465d
mbpolan <mike@mbpolan.com>
2021-10-02 18:41:44 -0400

    Initial Commit

---
+++ Serac/SeracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeracApp.swift
+//  Serac
+//
+//  Created by Mike Polan on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SeracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
